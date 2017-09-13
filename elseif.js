b971dc85f68f85c80d65ce395e0a9bff2c75a0e8
Damian Guillory <guillorywdamian@gmail.com>
2017-09-13 01:42:46 +0000

    automatically backed up by learn

--- elseif.js
+++ elseif.js
@@ -4,6 +4,8 @@ if (dog === "hungry") {
     console.log("Refilling food bowl.")
 } else if (dog === "thirsty") {
     console.log("Refilling water bowl.")
+} else if (dog === "playful") {
+    console.log("Playing tug of war")
 }
 else {
     console.log("Reading newspaper.")
